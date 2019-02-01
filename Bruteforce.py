2f060a631cf122cf0dd8c2d491507847b4802f47
Saquib Anwar <saquibedu@gmail.com>
2019-02-01 22:18:20 +0530

    Update Bruteforce.py

--- Bruteforce.py
+++ Bruteforce.py
@@ -2,3 +2,15 @@ import requests
 
 link1="https://"
 link2=".facebook.com"
+
+for i in range(97,123):
+	for j in range(97,123):
+		for k in range(97,123):
+			for l in range(97,123):
+				for m in range(97,123):
+					try:
+						print("testing -> "+link1+chr(i)+chr(j)+chr(k)+chr(l)+chr(m)+link2)
+						requests.get(link1+chr(i)+chr(j)+chr(k)+chr(l)+chr(m)+link2)
+
+					except:
+						print("Doesn't exists")
